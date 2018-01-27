c5a7c9cf09377022a5bb57d6b50d32518c8dbee4
navib <ivan.baena.dev@gmail.com>
2018-01-27 13:24:59 -0500

    initialized

---
+++ imports/ui/components/Preloader/preloader.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {TweenMax, Power2, TimelineLite} from "gsap";
+
+class Preloader extends Component {
+  constructor() {
+    super();
+
+  }
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="preload">JCS</div>
+    );
+  }
+}
+
+export default Preloader;
---
+++ imports/ui/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+//import SwiperBody from '../../ui/components/SwiperBody/swiperBody';
+
+
+// This component - represents a single page
+export const Home = () => <div>
+  <div className="jcs-home" id="desktop-sites">
+    Home
+  </div>
+</div>
